252e75bc7f37b0c616d146c824edd643c2429bcd
Leandro Bataglia <leandro.bataglia@gmail.com>
2024-08-01 16:53:05 -0300

    [chore] project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import './assets/main.css'
+</script>
+
+<template>
+  <header class="max-h-12">
+    <nav class="flex gap-2">
+      <a href="#" class="p-2 bg-gray-800"> Regular Operations </a>
+      <a href="#" class="p-2 bg-gray-800"> Integrated Strategies </a>
+      <a href="#" class="p-2 bg-gray-800"> Stationary Security Service </a>
+    </nav>
+  </header>
+  <main>
+
+  </main>
+</template>
